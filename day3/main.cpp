29d992c0b643b1bd9a30f2914a67bf7ad51626df
h wood <hwood@proton.me>
2024-12-24 12:20:35 +0000

    day3 p2 regex

--- day3/main.cpp
+++ day3/main.cpp
@@ -12,23 +12,34 @@ int main(int argc, char *argv[])
 
 	std::ifstream file("./day3.txt");
 	string line;
-	int num1 = 0, num2 = 0, total = 0;
-	regex str_expr_mul("(mul\\()[0-9]{1,3}\\,[0-9]{1,3}\\)");
+	int num1 = 0, num2 = 0, total = 0, flag = 1;
+	regex str_expr_mul(
+		"(mul\\()[0-9]{1,3}\\,[0-9]{1,3}\\)|(don't\\(\\))|(do\\(\\))");
 	regex str_expr_num("[0-9]{1,3}");
 	while (getline(file, line)) {
 		cout << line << endl;
 		for (smatch m; regex_search(line, m, str_expr_mul);) {
 			string mul_str = m.str();
 			line = m.suffix();
-			smatch num_match;
-			regex_search(mul_str, num_match, str_expr_num);
-			int num1 = stoi(num_match.str());
-			mul_str = num_match.suffix();
-			regex_search(mul_str, num_match, str_expr_num);
-			int num2 = stoi(num_match.str());
-			total += num1 * num2;
+			if (mul_str == "don't()")
+				flag = 0;
+			else if (mul_str == "do()")
+				flag = 1;
+			else {
+				if (flag) {
+					smatch num_match;
+					regex_search(mul_str, num_match,
+						     str_expr_num);
+					num1 = stoi(num_match.str());
+					mul_str = num_match.suffix();
+					regex_search(mul_str, num_match,
+						     str_expr_num);
+					num2 = stoi(num_match.str());
+					total += num1 * num2;
+				}
+			}
 			cout << num1 << " " << num2 << " " << num1 * num2 << " "
-			     << total << endl;
+			     << total << " " << flag << endl;
 		}
 	}
 	cout << "total: " << total << endl;
